7b667949da16c391d4dacd0ee873fa7694ca350a
Denis Berezin <nocab139@yandex.ru>
2021-12-14 16:18:45 +0300

    #3 api in next.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Heading from "../components/Heading";
 import styles from "../styles/Home.module.scss"
 import Head from "next/head";
+import Socials from "../components/Socials";
 
-const Home = () => {
+export const getStaticProps = async () => {
+    const response = await fetch(`${process.env.API_HOST}/socials/`)
+    const data = await response.json();
+
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {socials: data}
+    }
+};
+
+const Home = ({ socials }) => {
     return (
         <div className={styles.wrapper}>
             <Head>
@@ -10,6 +26,7 @@ const Home = () => {
             </Head>
             <Heading text={"General page on spa"} />
             <p>Text general</p>
+            <Socials socials={socials} />
         </div>
     )
 }
